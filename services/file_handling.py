a1b166f06ac9eee33c358b7cb6dac0b19ef4b68f
shuygena <i.am.zuzya@gmail.com>
2023-02-05 23:51:10 +0300

    add file handling

---
+++ services/file_handling.py
@@ -0,0 +1,17 @@
+from typing import Dict, Tuple
+
+BOOK_PATH = 'book/book.txt'
+PAGE_SIZE = 1050
+
+book: dict = {}
+
+
+def _get_part_text(text: str, start: int, size: int) -> Tuple:
+    pass
+
+
+def prepare_book(path: str) -> None:
+    pass
+
+
+prepare_book(BOOK_PATH)
