dd75a1180448e2a3d3bffbf58a1b870f3d543534
shuygena <i.am.zuzya@gmail.com>
2023-02-24 10:27:10 +0300

    redo _get_part_text and implement prepare_book

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from typing import Tuple
 
 BOOK_PATH = 'book/book.txt'
 PAGE_SIZE = 1050
@@ -6,25 +6,31 @@ PAGE_SIZE = 1050
 book: dict = {}
 
 
-def _get_part_text(text: str, start: int, page_size: int) -> Tuple:
-    ttext = text + ' '
-    pos1 = ttext.rfind(' ', start, start + page_size + 1)
-    pos2 = ttext.rfind('\n', start, start + page_size + 1)
-    sub_text = ttext[start:max(pos1, pos2)]
-    list_signs_positions = list()
-    list_signs_positions.append(sub_text.rfind(","))
-    list_signs_positions.append(sub_text.rfind("."))
-    list_signs_positions.append(sub_text.rfind("!"))
-    list_signs_positions.append(sub_text.rfind(":"))
-    list_signs_positions.append(sub_text.rfind(";"))
-    list_signs_positions.append(sub_text.rfind("?"))
-    pos = max(list_signs_positions) + 1
-    page = sub_text[:pos]
-    return page, len(page)
+def _get_part_text(text: str, start: int, size: int) -> Tuple:
+    chrs: str = ',.!:;?'
+    pg_size: int = size
+    if len(text) <= size + start:
+        pg_size = len(text) - start
+    else:
+        for i in range (size+start-1, start, -1):
+            if text[i] in chrs and text[i + 1] not in chrs:
+                break
+            pg_size -= 1
+    return [text[start: start + pg_size], pg_size]
+
 
 
 def prepare_book(path: str) -> None:
-    pass
+    f = open(path,'r')
+    text: str = f.read()
+    start: int = 0
+    end: int = len(text)
+    i: int = 1
+    while (start < end):
+        pg, delta = _get_part_text(text, start, PAGE_SIZE)
+        start += delta
+        book[i] = pg.lstrip(' \n\t')
+        i += 1
 
 
 prepare_book(BOOK_PATH)
